2258b879197924724750508e55409a15f4e688f8
Michael Greenly <mgreenly@gmail.com>
2011-03-11 21:13:20 -0600

    implemented #order

--- lib/datatable/datatable.rb
+++ lib/datatable/datatable.rb
@@ -1,7 +1,166 @@
 module Datatable
+
   class Datatable
-    def bark
-      "roooof"
+    attr_reader :controller, :action
+    attr_accessor :table
+
+    def self.build(model_class)
+      table = new(model_class)
+      yield(table)
+      table
     end
+
+    def initialize(model_class)
+      @model = model_class
+      @table = model_class.table_name
+      @columns = []
+      @view_stuff = []
+      @option = {
+        :bProcessing => true,
+        :bServerSide => true,
+        :sPaginationType => "full_numbers",
+        :iDisplayLength => 25,
+        :bLengthChange => true,
+        :bStateSave => false,
+        :bFilter => true,
+        :bAutoWidth => false,
+        :aaSorting => "[[0, 'ASC']]",
+        :bJQeuryUI => true,
+        :sAjaxSource => "/#{table}.js"
+      }
+    end
+
+    def []=(index,hash)
+      @view_stuff[index] = hash
+    end
+
+    # :name - attribute of the model
+    #
+    #    what if we wrote the name as association.field that should mean we can do
+    #    a away with the extractor we're passing in?
+    #
+    # :fetcher - this calls the :name method on each object to get it's contents
+    #            when the array is being built for display
+    #
+    #
+    def column(name, extractor=nil, fetcher=nil, type=nil)
+      @columns << [
+        name,
+        extractor ? extractor : "#{@table}.#{name}",
+        fetcher ? fetcher : lambda{|o| o.send(name) || "" },
+        type ? fetcher : :integer
+      ]
+    end
+
+    def script
+      <<-RESULT.gsub(/^\s{8}/,"")
+        <script type='text/javascript'>
+          $(document).ready(function() {
+            $('.datatable').dataTable({
+              sDom: '<"H"lr>t<"F"ip>',
+              oLanguage: {
+                sSearch: "Search",
+                sProcessing: "<img alt='Spinner' src='/images/spinner.gif'/>"
+              },
+              bJQueryUI: #{@option[:bJQeuryUI]},
+              sPaginationType: "#{@option[:sPaginationType]}",
+              iDisplayLength: #{@option[:iDisplayLength]},
+              bProcessing: #{@option[:bProcessing]},
+              bServerSide: #{@option[:bServerSide]},
+              sAjaxSource: "#{@option[:sAjaxSource]}",
+              bLengthChange: #{@option[:bLengthChange]},
+              bStateSave: #{@option[:bStateSave]},
+              bFilter: #{@option[:bFilter]},
+              bAutoWidth: #{@option[:bAutoWidth]},
+              aaSorting: #{@option[:aaSorting]},
+             });
+          });
+        </script>
+      RESULT
+    end
+
+    #
+    #
+    #
+    #
+    def html
+      result =<<-CONTENT.gsub(/^\s{8}/,"")
+        <div class='datatable_container'>
+          <table class='datatable'>
+            <thead>
+              <tr>
+      CONTENT
+      @columns.each do |column|
+        result << "        <th>#{column[0]}</th>\n"
+      end
+      result << <<-CONTENT.gsub(/^\s{8}/,"")
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      CONTENT
+      result
+    end
+
+    def render
+      (html + "\n" + script).html_safe
+    end
+
+    def array_of(data)
+      result = []
+      @columns.each do |column|
+        result << column[2].call(data)
+      end
+      result
+    end
+
+    def conditions(params)
+      ""
+    end
+
+    def count(params)
+      @model.count
+    end
+
+    def page(params)
+      (params[:iDisplayStart].to_i/params[:iDisplayLength].to_i rescue 0)+1
+    end
+
+    # iSortingCols  - gives number of columns being sorted
+    # iSortCol_0    - gives the index of the highest precedent column for sorting (the value is zero to n-1 columns)
+    # sSortDir_0    - gives the direction of the highest precedent column for sorting (asc or desc)
+    # iSortCol_1    - gives the index of the next highest precedent column for sorting
+    # sSortDir_1    - gives the direction of next the highest precedent column for sorting
+    def order(params)
+      result = []
+      1.upto(params[:iSortingCols].to_i) do |count|
+        cur_sort_col = "iSortCol_#{count - 1}".to_sym
+        cur_sort_dir = "sSortDir_#{count - 1}".to_sym
+        col_name = @columns[params[cur_sort_col].to_i][1]
+        col_direction  = params[cur_sort_dir].upcase
+        result << "#{col_name} #{col_direction}"
+      end
+      result.join(", ")
+    end
+
+    def paginate(params)
+      @model.paginate(
+        :page => page(params),
+        :order => order(params),
+        :per_page => params[:iDisplayLength] )
+    end
+
+    def json(params)
+      data = paginate(params)
+      total_objects = count(params)
+      { 'sEcho' => params[:sEcho].to_i || -1,
+        'iTotalRecords' => total_objects,
+        'iTotalDisplayRecords'=> total_objects,
+        'aaData' => data.map{|e| array_of(e)}
+      }.to_json
+    end
+    
   end
 end
--- spec/datatable_spec.rb
+++ spec/datatable_spec.rb
@@ -1,12 +1,64 @@
 require 'datatable'
 
 describe Datatable do
-  it "creates new instance given valid attributes" do
-    datatable = Datatable::Datatable.new
-    datatable.should_not be_nil
-  end
-  it "bark says roof" do
-    datatable = Datatable::Datatable.new
-    datatable.bark.should == "roooof"
+  describe 'correctly orders' do
+
+    before(:each) do
+      @datatable = Datatable::Datatable.build(stub(Object, :table_name => 'table')) do |table|
+        table.column :col1
+        table.column :col2
+        table.column :col3
+      end
+    end
+
+    # "iSortingCols"=>"1",
+    it "nothing" do
+      @datatable.order({:iSortingCols => 0}).should == ""
+    end
+
+    # "iSortingCols"=>"1", "iSortCol_0"=>"0", "sSortDir_0"=>"asc"
+    it "column 1 ascending" do
+      @datatable.order({:iSortingCols => 1, :iSortCol_0 => 0, :sSortDir_0 => 'asc'}).should == 'table.col1 ASC'
+    end
+
+    # "iSortingCols"=>"1", "iSortCol_0"=>"0", "sSortDir_0"=>"desc",
+    it "column 1 descending" do
+      @datatable.order({:iSortingCols => 1, :iSortCol_0 => 0, :sSortDir_0 => 'desc'}).should == 'table.col1 DESC'
+    end
+
+    # "iSortingCols"=>"1", "iSortCol_0"=>"1", "sSortDir_0"=>"desc",
+    it "column 2 ascending" do
+      @datatable.order({:iSortingCols => 1, :iSortCol_0 => 1, :sSortDir_0 => 'asc'}).should == 'table.col2 ASC'
+    end
+
+    # "iSortingCols"=>"1", "iSortCol_0"=>"1", "sSortDir_0"=>"desc",
+    it "column 2 descending" do
+      @datatable.order({:iSortingCols => 1, :iSortCol_0 => 1, :sSortDir_0 => 'desc'}).should == 'table.col2 DESC'
+    end
+
+    # "iSortingCols"=>"2", "iSortCol_0"=>"0", "sSortDir_0"=>"asc", "iSortCol_1"=>"1", "sSortDir_1"=>"asc",
+    it "column 1 ascending, column 2 ascending" do
+      @datatable.order({:iSortingCols => 2, :iSortCol_0 => 0, :sSortDir_0 => 'asc', :iSortCol_1 => 1, :sSortDir_1 => 'asc' }).should == 'table.col1 ASC, table.col2 ASC'
+    end
+    
+    # "iSortingCols"=>"2", "iSortCol_0"=>"0", "sSortDir_0"=>"desc", "iSortCol_1"=>"1", "sSortDir_1"=>"desc",
+    it "column 1 descending, column 2 descending" do
+      @datatable.order({:iSortingCols => 2, :iSortCol_0 => 0, :sSortDir_0 => 'desc', :iSortCol_1 => 1, :sSortDir_1 => 'desc' }).should == 'table.col1 DESC, table.col2 DESC'
+    end
+
+    # "iSortingCols"=>"2", "iSortCol_0"=>"0", "sSortDir_0"=>"asc", "iSortCol_1"=>"2", "sSortDir_1"=>"desc",
+    it "column 1 ascending, column 3 descending" do
+      @datatable.order({:iSortingCols => 2, :iSortCol_0 => 0, :sSortDir_0 => 'asc', :iSortCol_1 => 2, :sSortDir_1 => 'desc' }).should == 'table.col1 ASC, table.col3 DESC'
+    end
+
+    # "iSortingCols"=>"2", "iSortCol_0"=>"0", "sSortDir_0"=>"desc", "iSortCol_1"=>"2", "sSortDir_2"=>"asc",
+    it "column 1 descending, column 3 ascending" do
+      @datatable.order({:iSortingCols => 2, :iSortCol_0 => 0, :sSortDir_0 => 'desc', :iSortCol_1 => 2, :sSortDir_1 => 'asc' }).should == 'table.col1 DESC, table.col3 ASC'
+    end
+    
+    it "column 2 ascending, column 1 ascending" do
+      @datatable.order({:iSortingCols => 2, :iSortCol_0 => 1, :sSortDir_0 => 'asc', :iSortCol_1 => 0, :sSortDir_1 => 'asc' }).should == 'table.col2 ASC, table.col1 ASC'
+    end
+
   end
 end

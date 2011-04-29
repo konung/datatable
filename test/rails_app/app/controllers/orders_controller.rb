ee201dd74aa15a7d8161c8d736909111b7933307
Mischa <f.mischa@gmail.com>
2011-04-29 13:00:02 -0700

    basic setup

--- test/rails_app/app/controllers/orders_controller.rb
+++ test/rails_app/app/controllers/orders_controller.rb
@@ -1,7 +1,27 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = Order.limit(10)
+    respond_to do |format|
+      format.html { @orders = Order.limit(10) }
+
+      format.js do 
+        json = {
+          'sEcho' =>  -1,
+          'iTotalRecords' => 0,
+          'iTotalDisplayRecords' => 0,
+          'aaData' => []
+        }
+
+        render :json => json.to_json #Order.limit(10).to_json
+      end
+
+    end
   end
+
+  #  { 'sEcho' => params[:sEcho].to_i || -1,
+  #   'iTotalRecords' => total_objects,
+  #   'iTotalDisplayRecords'=> total_objects,
+  #   'aaData' => data.map{|e| array_of(e)}
+  # }.to_json
 
 end

da5ccd3ef33902d5d0afe26b709ccf8b08f5e95f
Mischa <f.mischa@gmail.com>
2011-05-13 13:40:18 -0700

    starting on sql /arel generation

--- example_app/spec/data_tables/orders_index_spec.rb
+++ example_app/spec/data_tables/orders_index_spec.rb
@@ -2,16 +2,24 @@ require 'spec_helper'
 
 describe OrdersIndex do
 
-  it 'should work' do
-    Order.create!(:order_number => 32)
-    data_table = OrdersIndex.new
-    query = {}
-    json = data_table.query(params).json
-    json['aaRecords'][0][0].should == 32
+# it 'should work' do
+#   Order.create!(:order_number => 32)
+#   data_table = OrdersIndex.new
+#   params = {}
+#   json = data_table.query(params).json
+#   json['aaRecords'][0][0].should == 32
+# end
+
+  it 'should store a partial sql query' do
+    class OrdersSimple < DataTable
+      model Order 
+      column :order_number
+    end
+
+    data_table = OrdersSimple.new
+    data_table.sql.should == 'SELECT order_number FROM orders'
   end
 
-  it 'should return json' do
 
-  end
 
 end
--- lib/datatable.rb
+++ lib/datatable.rb
@@ -1,9 +1,33 @@
 class DataTable
+  # include SQLGenerator
+  # include ParamsParser
 
   attr_accessor :echo
   attr_accessor :data
   attr_accessor :total_count
   attr_accessor :displayed_count
+
+  def self.column(*args)
+  end
+
+  def self.join(*args)
+  end
+
+  def self.model(model_name)
+    @@model_name = model_name
+  end
+
+  # 1. data table subclass is initialized 
+  #   class methods are going to be called on the class instance, and the class will store table data
+  #
+  # 2. instantiate an instance of the data table subclass ( OrdersIndex.new)
+  #
+  # 3. query the instance w/ pagination and sorting params
+  #    a query gets executed w/ params -> ARel
+  #    results get stored  -> AR
+  #    results get passed back as json
+
+
 
   def initialize(params={})
     @echo = (params['sEcho'] || -1).to_i
@@ -17,8 +41,14 @@ class DataTable
       'sEcho' => echo,
       'aaData' => data,
       'iTotalRecords' => total_count,
-      'iTotalDisplayRecords' => displayed_count
+      'iTotalDisplayRecords' => displayed_count,
+      'aaRecords' => []
     }
   end
+
+  def query(params)
+    self
+  end
+
 
 end

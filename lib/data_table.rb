094c67852227d162e40de383bea72538d1b36aa3
Michael Greenly <mgreenly@gmail.com>
2011-06-16 13:32:11 -0500

    refactored instance portion of datatable

--- lib/data_table.rb
+++ lib/data_table.rb
@@ -24,6 +24,14 @@ module DataTable
 
     attr_accessor :records
 
+    def self.sql(s)
+      @sql_string = s
+    end
+
+    def self.sql_string
+      @sql_string
+    end
+
     def self.columns
       raise 'There are no columns on the DataTable (use assign_column_names)' unless @columns
       @columns
@@ -67,98 +75,58 @@ module DataTable
       @javascript_options || {}
     end
 
-
-
-
-    #------------------------------------------------------------------------------------------------------------------
-    #  type       name                  description
-    #------------------------------------------------------------------------------------------------------------------
-    #  int        iDisplayStart       Display start point
-    #
-    #  int        iDisplayLength      Number of records to display
-    #
-    #  int        iColumns            Number of columns being displayed (useful for getting individual column search info)
-    #
-    #  string     sSearch             Global search field
-    #
-    #  boolean    bEscapeRegex        Global search is regex or not
-    #
-    #  boolean    bSortable_(int)     Indicator for if a column is flagged as sortable or not on the client-side
-    #
-    #  boolean    bSearchable_(int)   Indicator for if a column is flagged as searchable or not on the client-side
-    #
-    #  string     sSearch_(int)	      Individual column filter
-    #
-    #  boolean    bEscapeRegex_(int)	Individual column filter is regex or not
-    #
-    #  int        iSortingCols	      Number of columns to sort on
-    #
-    #  int        iSortCol_(int)	    Column being sorted on (you will need to decode this number for your database)
-    #
-    #  string     sSortDir_(int)	    Direction to be sorted - "desc" or "asc". Note that the prefix for this variable
-    #                                 is wrong in 1.5.x where iSortDir_(int) was used)
-    #
-    #  string     sEcho	              Information for DataTables to use for rendering
-    #
-    #-----------------------------------------------------------------------------------------------------------------
     def self.query(params)
       params.each do |key, value|
         params[key] = value.to_i if key =~ /^i/
       end
-
       datatable = new(params)
-      datatable.query
+      datatable.instance_query
       datatable
     end
 
-    # Private
+    # only used in testing
     def self.to_sql
       relation.to_sql
     end
+
 
     def initialize(params={})
       @params = params
       @records = []
     end
 
-    #------------------------------------------------------------------------------------------------------------------
-    #  type       name                  description
-    #------------------------------------------------------------------------------------------------------------------
-    #  int        iTotalRecords         Total records, before filtering (i.e. the total number of records in
-    #                                   the database)
-    #
-    #  int        iTotalDisplayRecords  Total records, after filtering (i.e. the total number of records after
-    #                                   filtering has been applied - not just the number of records being returned
-    #                                   in this result set)
-    #
-    #  string     sEcho                 An unaltered copy of sEcho sent from the client side. This parameter will
-    #                                   change with each draw (it is basically a draw count) - so it is important
-    #                                   that this is implemented. Note that it strongly recommended for security
-    #                                   reasons that you 'cast' this parameter to an integer in order to prevent
-    #                                   Cross Site Scripting (XSS) attacks.
-    #
-    #  string     sColumns              Optional - this is a string of column names, comma separated (used in
-    #                                   combination with sName) which will allow DataTables to reorder data on the
-    #                                   client-side if required for display
-    #
-    #  array      aaData                The data in a 2D array
-    #-----------------------------------------------------------------------------------------------------------------
+    def instance_query
+      @records =  self.class.sql_string ? sql_instance_query : active_record_instance_query
+      self
+    end
+    
     def to_json
       {
         'sEcho' => (@params['sEcho'] || -1).to_i,
         'aaData' => records,
-        'iTotalRecords' => @records.length, 
+        'iTotalRecords' => @records.length,
         #self.class.model.count,
         'iTotalDisplayRecords' => records.length,
       }
     end
 
-    def self.sql(s)
-      @sql_string = s
+    private
+
+    def sql_instance_query
+      current_sql = query_sql
+      connection = self.class.model ? self.class.model.connection : ActiveRecord::Base.connection
+      connection.select_rows(current_sql)
     end
 
-    def self.sql_string
-      @sql_string
+    def active_record_instance_query
+      raise "set_model not called on #{self.class.name}" unless self.class.model
+      relation = self.class.relation
+      if(search = search_string)
+        relation = relation.where(search)
+      end
+      relation = relation.order(order_string) if @params['iSortingCols'].to_i > 0
+      relation = relation.offset(@params['iDisplayStart']).limit(@params['iDisplayLength'])
+      self.class.model.connection.select_rows(relation.to_sql)
     end
 
     #
@@ -240,25 +208,6 @@ module DataTable
       end
       current_sql << (" ORDER BY" + order_string) if @params['iSortingCols'].to_i > 0
       current_sql << limit_offset
-    end
-
-    def query
-      if self.class.sql_string
-        current_sql = query_sql
-        connection = self.class.model ? self.class.model.connection : ActiveRecord::Base.connection
-        @records = connection.select_rows(current_sql)
-
-      else
-        raise "set_model not called on #{self.class.name}" unless self.class.model
-        relation = self.class.relation
-        if(search = search_string)
-          relation = relation.where(search)
-        end
-        relation = relation.order(order_string) if @params['iSortingCols'].to_i > 0
-        relation = relation.offset(@params['iDisplayStart']).limit(@params['iDisplayLength'])
-        @records = self.class.model.connection.select_rows(relation.to_sql)
-      end
-      self
     end
 
   end

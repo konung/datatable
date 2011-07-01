2fe4cd9faf35da3cded8226601615ec5957af780
Michael Greenly <mgreenly@gmail.com>
2011-07-01 16:10:36 -0500

    cleaned up the spec

--- example_app/spec/datatables/sql_pagination_spec.rb
+++ example_app/spec/datatables/sql_pagination_spec.rb
@@ -1,97 +1,114 @@
 require 'spec_helper'
 
-describe 'manual sql pagination' do
+describe 'pagination with count()' do
 
   before do
-
     Object.send(:remove_const, :T) rescue nil
-
     class T < Datatable::Base
-
-      count <<-SQL
-       SELECT COUNT(*) + 100 FROM orders
-      SQL
-
-      sql <<-SQL
-        SELECT id FROM orders
-      SQL
-
-      columns(
-        {"orders.id" => {:type => :integer, :link_to => link_to('{{0}}', order_path('{{0}}')) }}
-      )
+      count("SELECT COUNT(*) + 100 FROM orders")
+      sql("SELECT id FROM orders")
+      columns({"orders.id" => {:type => :integer, :link_to => link_to('{{0}}', order_path('{{0}}')) }})
     end
-
     @params = {
       'iColumns' => 1,
       "bSearchable_0" => true
     }
-
     4.times{ Factory(:order) }
   end
 
-  it "should provide first page" do
-    @params['iDisplayStart'] = 0
-    @params['iDisplayLength'] = 2
+  describe "first page" do
+    before do
+      @params['iDisplayStart'] = 0
+      @params['iDisplayLength'] = 2
+    end
 
-    T.query(@params).to_json['iTotalRecords'].should == 2
-    T.query(@params).to_json['iTotalDisplayRecords'].should == 104 # + 100 to make sure we are actually using the count 
-    T.query(@params).to_json['aaData'].should == Order.all[0..1].map {|o| [o.id.to_s] }
+    it "iTotalRecords correctly set" do
+      T.query(@params).to_json['iTotalRecords'].should == 2
+    end
+
+    it "iTotalDisplayRecords correctly set" do
+      T.query(@params).to_json['iTotalDisplayRecords'].should == 104
+    end
+
+    it "aaData contains dataset" do
+      T.query(@params).to_json['aaData'].should == Order.all[0..1].map {|o| [o.id.to_s] }
+    end
   end
 
+  describe "second page" do
+    before do
+      @params['iDisplayStart'] = 2
+      @params['iDisplayLength'] = 2
+    end
 
-  it "should provide second page" do
-    @params['iDisplayStart'] = 2
-    @params['iDisplayLength'] = 2
+    it "iTotalRecords correctly set" do
+      T.query(@params).to_json['iTotalRecords'].should == 2
+    end
 
-    T.query(@params).to_json['iTotalRecords'].should == 2
-    T.query(@params).to_json['iTotalDisplayRecords'].should == 104
-    T.query(@params).to_json['aaData'].should == Order.all[2..3].map {|o| [o.id.to_s] }
+    it "iTotalDisplayRecords correctly set" do
+      T.query(@params).to_json['iTotalDisplayRecords'].should == 104
+    end
+
+    it "aaData contains dataset" do
+      T.query(@params).to_json['aaData'].should == Order.all[2..3].map {|o| [o.id.to_s] }
+    end
   end
-
 end
 
 
-describe 'automatic sql pagination' do
 
+
+describe 'pagination without count()' do
+  
   before do
-
     Object.send(:remove_const, :T) rescue nil
-
     class T < Datatable::Base
-      sql <<-SQL
-        SELECT id FROM orders
-      SQL
-
-      columns(
-        {"orders.id" => {:type => :integer, :link_to => link_to('{{0}}', order_path('{{0}}')) }}
-      )
+      sql("SELECT id FROM orders")
+      columns({"orders.id" => {:type => :integer, :link_to => link_to('{{0}}', order_path('{{0}}')) }})
     end
-
     @params = {
       'iColumns' => 1,
       "bSearchable_0" => true
     }
-
     4.times{ Factory(:order) }
   end
 
-  it "should provide first page" do
-    @params['iDisplayStart'] = 0
-    @params['iDisplayLength'] = 2
+  describe "first page" do
+    before do
+      @params['iDisplayStart'] = 0
+      @params['iDisplayLength'] = 2
+    end
 
-    T.query(@params).to_json['iTotalRecords'].should == 2
-    T.query(@params).to_json['iTotalDisplayRecords'].should == 4
-    T.query(@params).to_json['aaData'].should == Order.all[0..1].map {|o| [o.id.to_s] }
+    it "iTotalRecords correctly set" do
+      T.query(@params).to_json['iTotalRecords'].should == 2
+    end
+
+    it "iTotalDisplayRecords correctly set" do
+      T.query(@params).to_json['iTotalDisplayRecords'].should == 4
+    end
+
+    it "aaData contains dataset" do
+      T.query(@params).to_json['aaData'].should == Order.all[0..1].map {|o| [o.id.to_s] }
+    end
   end
 
 
-  it "should provide second page" do
-    @params['iDisplayStart'] = 2
-    @params['iDisplayLength'] = 2
+  describe "second page" do
+    before do
+      @params['iDisplayStart'] = 2
+      @params['iDisplayLength'] = 2
+    end
+    it "iTotalRecords correctly set" do
+      T.query(@params).to_json['iTotalRecords'].should == 2
+    end
 
-    T.query(@params).to_json['iTotalRecords'].should == 2
-    T.query(@params).to_json['iTotalDisplayRecords'].should == 4
-    T.query(@params).to_json['aaData'].should == Order.all[2..3].map {|o| [o.id.to_s] }
+    it "iTotalDisplayRecords correctly set" do
+      T.query(@params).to_json['iTotalDisplayRecords'].should == 4
+    end
+
+    it "aaData contains dataset" do
+       T.query(@params).to_json['aaData'].should == Order.all[2..3].map {|o| [o.id.to_s] }
+    end
   end
 
 end

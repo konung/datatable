2ff42920d1c2ddb44fa3ef9a55e2fb8b7a27458c
Mischa <f.mischa@gmail.com>
2011-07-10 11:29:29 -0500

    Fix production mode

--- example_app/app/datatables/orders_index.rb
+++ example_app/app/datatables/orders_index.rb
@@ -1,15 +1,6 @@
-# TODO: fix this!!!
-#
-# if the code below is evaluated before the tests are run it will throw
-# "method_missing": undefined method "order_path" but it works just fine
-# in development or production mode.  Only evaluating it when we're not
-# testing will let us ignore this for the time being. 
-#
-unless Rails.env =~ /test/
+class OrdersIndex < Datatable::Base
 
-  class OrdersIndex < Datatable::Base
-
-    sql <<-SQL
+  sql <<-SQL
       SELECT
         orders.id,
         orders.order_number,
@@ -20,23 +11,21 @@ unless Rails.env =~ /test/
         orders
       JOIN
         customers ON customers.id = orders.customer_id
-    SQL
+  SQL
 
-    columns(
-      {"orders.id" => {:type => :integer, :sTitle => "Id", :sWidth => '50px'}},
-      {"orders.order_number" => {:type => :integer, :link_to => link_to('{{1}}', order_path('{{0}}')),:sTitle => 'Order Number', :sWidth => '125px'  }},
-      {"customers.first_name" => {:type => :string, :link_to => link_to('{{2}}', order_path('{{0}}')),:sWidth => '200px' }},
-      {"customers.last_name" => {:type => :string,:sWidth => '200px'}},
-      {"orders.memo" => {:type => :string }}
-    )
-    option('bJQueryUI', true)
-    option('individual_column_searching', true)
-    #option('sDom', '<"H"lrf>t<"F"ip>')    # use with pagination
-    # to use pagination comment out following and enable previous line
-    option('sDom', '<"clear"><"H"Trf>t<"F"i>')
-    option('bScrollInfinite', true)
-    option('bScrollCollapse', true)
-    option('sScrollY', '200px')
-  end
+  columns(
+    {"orders.id" => {:type => :integer, :sTitle => "Id", :sWidth => '50px'}},
+    {"orders.order_number" => {:type => :integer, :link_to => link_to('{{1}}', order_path('{{0}}')),:sTitle => 'Order Number', :sWidth => '125px'  }},
+    {"customers.first_name" => {:type => :string, :link_to => link_to('{{2}}', order_path('{{0}}')),:sWidth => '200px' }},
+    {"customers.last_name" => {:type => :string,:sWidth => '200px'}},
+    {"orders.memo" => {:type => :string }}
+  )
+  option('bJQueryUI', true)
+  option('individual_column_searching', true)
+  #option('sDom', '<"H"lrf>t<"F"ip>')    # use with pagination
+  # to use pagination comment out following and enable previous line
+  option('sDom', '<"clear"><"H"Trf>t<"F"i>')
+  option('bScrollInfinite', true)
+  option('bScrollCollapse', true)
+  option('sScrollY', '200px')
 end
-
--- lib/datatable/railtie.rb
+++ lib/datatable/railtie.rb
@@ -3,8 +3,15 @@ require 'rails'
 
 module Datatable
   class Railtie < ::Rails::Railtie
+
+    initializer 'datatable.load_later' do
+      datatable_path = Rails.application.config.eager_load_paths.grep(/app\/datatable/)
+      Rails.application.config.eager_load_paths -= datatable_path
+    end
+
     initializer "datatable.helper" do
       ActionView::Base.send :include, Datatable::Helper
     end
+
   end
 end
